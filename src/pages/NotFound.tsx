402ec08003e1f7adc076c1289156dc9de0f0ca6f
V1ktorkk <ya-viktor44444@yandex.ru>
2025-10-28 01:20:05 +0300

    Build fix

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './notFoundPage.module.css';
+import styles from '../styles/notFoundPage.module.css';
 
 const NotFoundPage: FC = () => (
   <div className="page">
